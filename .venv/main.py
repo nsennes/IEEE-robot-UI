69a5537dbe06c6d2c4f50e98f5798a5776e71e13
nsennes <panita.chavik@gmail.com>
2023-09-02 18:10:23 +0700

    created classes, mood loops

--- .venv/main.py
+++ .venv/main.py
@@ -1,68 +1,72 @@
 import pygame
 from sys import exit
-from random import randint
+import time
 
+# Initialize pygame and set up the display
 pygame.init()
-screen = pygame.display.set_mode((800,480))
+screen = pygame.display.set_mode((800, 480))
 clock = pygame.time.Clock()
+status = True
 
+class FaceAnimation:
+    def __init__(self, screen, face_list, animation_speed):
+        self.screen = screen
+        self.face_list = face_list
+        self.animation_speed = animation_speed
+        self.index = 0
+        self.face_surf = face_list[self.index]
 
+    def animate(self):
+        self.index += self.animation_speed
+        if self.index >= len(self.face_list):
+            self.index = 0
+        self.face_surf = self.face_list[int(self.index)]
+        self.screen.blit(self.face_surf, (0, 0))
 
-#angryFace
-angry_face_list = [pygame.transform.scale(pygame.image.load(f'graphics/angryFace/{i}.png'), (800, 480)) for i in range(1, 15)]
-af_index = 0
-angry_face_surf = angry_face_list[af_index]
-af_animation_speed = 0.5
+class Mood:
+    def __init__(self):
+        # Initialize face animations for different moods
+        self.moods = {}
+    
+    def add_mood(self, name, face_list, animation_speed):
+        self.moods[name] = FaceAnimation(screen, face_list, animation_speed)
 
-#angryFace function
-def angryFaceAnimation(screen, angry_face_list, af_animation_speed):
-    global angry_face_surf, af_index
-    af_index += af_animation_speed
-    if af_index >= len(angry_face_list):af_index = 0
-    angry_face_surf = angry_face_list[int(af_index)]
-    screen.blit(angry_face_surf, (0, 0))  # Draw each frame of the angry face
+    def animate(self, mood_name):
+        if mood_name in self.moods:
+            self.moods[mood_name].animate()
+        else:
+            print(f"Mood '{mood_name}' not found")
 
+    def reset(self):
+        self.animate("neutral") 
 
-#happyFace
-happy_face_list = [pygame.transform.scale(pygame.image.load(f'graphics/happyFace/{i}.png'), (800,480)) for i in range(1,17)]
-hf_index = 0
-happy_face_surf = happy_face_list[hf_index]
-hf_animation_speed = 0.35
+# Create a Mood instance
+mood = Mood()
 
-#happyFace function
-def happyFaceAnimation(screen, happy_face_list, hf_animation_speed):
-    global happy_face_surf, hf_index
-    hf_index += hf_animation_speed
-    if hf_index >= len(happy_face_list): hf_index = 0
-    happy_face_surf = happy_face_list[int(hf_index)]
-    screen.blit(happy_face_surf,(0,0))
+def setup_moods():
+    # Add different moods with their respective animations
+    angry_face_list = [pygame.transform.scale(pygame.image.load(f'graphics/angryFace/{i}.png'), (800, 480)) for i in range(1, 15)]
+    mood.add_mood("angry", angry_face_list, 0.5)
 
+    happy_face_list = [pygame.transform.scale(pygame.image.load(f'graphics/happyFace/{i}.png'), (800, 480)) for i in range(1, 18)]
+    mood.add_mood("happy", happy_face_list, 0.35)
 
-#neutralFace
-neutral_face_list = [pygame.transform.scale(pygame.image.load(f'graphics/neutralFace/{i}.png'), (800,480)) for i in range(1,23)]
-nf_index = 0
-neutral_face_surf = neutral_face_list[nf_index]
-nf_animation_speed = 0.6
+    neutral_face_list = [pygame.transform.scale(pygame.image.load(f'graphics/neutralFace/{i}.png'), (800, 480)) for i in range(1, 24)]
+    mood.add_mood("neutral", neutral_face_list, 0.6)
 
-#neutralFace function
-def neutralFaceAnimation(screen, neutral_face_list, nf_animation_speed):
-    global neutral_face_surf, nf_index
-    nf_index += nf_animation_speed
-    if nf_index >= len(neutral_face_list): nf_index = 0
-    neutral_face_surf = neutral_face_list[int(nf_index)]
-    screen.blit(neutral_face_surf,(0,0))
+def mood_loop(mood_name, status, duration):
+    start_ticks = pygame.time.get_ticks()  # starter tick
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
 
+        if status:
+            mood.animate(mood_name)
+            if duration != -1 and pygame.time.get_ticks() - start_ticks > duration:
+                break
 
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
-    #angryFaceAnimation(screen, angry_face_list, af_animation_speed)
-    #happyFaceAnimation(screen, happy_face_list, hf_animation_speed)
-    neutralFaceAnimation(screen, neutral_face_list, nf_animation_speed)
-
-    pygame.display.update()
-    clock.tick(30) #this is to control the amount of framerates in the animation
+            
+        pygame.display.update()
+        clock.tick(30)  # Control the frame rate
